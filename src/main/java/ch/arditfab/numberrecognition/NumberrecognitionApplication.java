1be7e01255a4d8b0757d5e8b5c650b31e9cf3a3f
FabioArdi <arditfab@students.zhaw.ch>
2023-05-22 19:46:23 +0200

    First commit

---
+++ src/main/java/ch/arditfab/numberrecognition/NumberrecognitionApplication.java
@@ -0,0 +1,13 @@
+package ch.arditfab.numberrecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberrecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberrecognitionApplication.class, args);
+	}
+
+}
